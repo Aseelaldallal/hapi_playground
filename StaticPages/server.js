00e62aaa788c3766210f0058cc283022db4597f7
aseelaldallal <aseelaldallal@gmail.com>
2018-03-17 12:32:33 -0400

    server to serve static pages

---
+++ StaticPages/server.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const Hapi = require("hapi");
+
+// Create a server with host and port
+const server = Hapi.server({
+  host: "localhost",
+  port: 3000
+});
+
+// Add the route
+server.route({
+  method: "GET",
+  path: "/",
+  handler: (request, h) => "hello world"
+});
+
+// start the server
+const init = async () => {
+  await server.register(require("inert"));
+
+  server.route({
+    method: "GET",
+    path: "/hello",
+    handler: (req, h) => h.file("./public/hello.html")
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
